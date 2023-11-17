1f48d83252ebbf8ab1d0a61757eb53da55b2a6d4
Ayush Sharma <ayushsharma830230@gmail.com>
2023-11-17 11:52:14 +0530

    copy to clipboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback, useEffect } from "react"
+import { useState, useCallback, useEffect, useRef } from "react"
 
 function App() {
-  const [password, setPassword] = useState()
+  const [password, setPassword] = useState("")
   const [length, setLength] = useState(5)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [characterAllowed, setCharacterAllowed] = useState(false)
+
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -20,6 +22,11 @@ function App() {
     setPassword(pass)
   }, [length, numberAllowed, characterAllowed])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, characterAllowed, passwordGenerator])
@@ -28,12 +35,12 @@ function App() {
     <>
       <div class="p-2 m-2 h-fit w-fit bg-gray-400">
         <div>
-          <input type="text" placeholder="password" class="p-1 border-black rounded " value={password} />
-          <button class="bg-blue-500 hover:bg-blue-700 rounded p-1 font-bold text-white">Copy</button>
+          <input type="text" placeholder="password" ref={passwordRef} class="p-1 border-black rounded " value={password} />
+          <button onClick={copyPasswordToClipboard} class="bg-blue-500 hover:bg-blue-700 rounded p-1 font-bold text-white">Copy</button>
         </div>
         <div>
           <span>
-            <input type="range" min="1" max="20" value={length} class="custom-slidor" onChange={(e) => { setLength(e.target.value) }} />
+            <input type="range" min="1" max="20" value={length} class="custom-slidor" readOnly onChange={(e) => { setLength(e.target.value) }} />
             <label htmlFor="length">Length : {length}</label>
           </span>
           <span class="m-1">
